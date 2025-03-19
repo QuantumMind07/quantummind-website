d03fc12d6704a5e566d5a1de6b59d3216ff17cbe
jayshah0726 <jay.shah@elixirequities.com>
2025-03-19 11:09:38 +0530

    Added about page content

--- frontend/src/app/About/main.js
+++ frontend/src/app/About/main.js
@@ -1,12 +1,21 @@
-"use client"
-import React from 'react'
+// main.js
+"use client";
+import React from 'react';
 
-const Main = () => {
+const Main = ({ children }) => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       {console.log("about")}
+
+      {/* Main Content */}
+      <main className="flex-grow container mx-auto p-6">{children}</main>
+
+      {/* Footer */}
+      <footer className="bg-gray-900 text-white p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Quantum Mind. All Rights Reserved.</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
